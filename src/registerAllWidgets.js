120f1aae03c863fbc84ad5309ce816f8e0ffb5c6
jhsware <sebastian@urbantalk.se>
2015-07-16 16:57:01 +0200

    Fix

--- src/registerAllWidgets.js
+++ src/registerAllWidgets.js
@@ -3,22 +3,24 @@ var widgets = require('./formInputWidgets');
 var registry = require('protoncms-core').registry;
 
 // Registering adapters for form input widgets
-for (var key in widgets.adapters) {
-    try {
-        registry.registerAdapter(widgets.adapters[key]);
-    } catch (e) {
-        console.error("Couldn't register: " + key);
-        throw e;
-    }
+module.exports = function () {
+    for (var key in widgets.adapters) {
+        try {
+            registry.registerAdapter(widgets.adapters[key]);
+        } catch (e) {
+            console.error("Couldn't register: " + key);
+            throw e;
+        }
     
-}
+    }
 
-for (var key in widgets.utilities) {
-    try {
-        registry.registerUtility(widgets.utilities[key]);
-    } catch (e) {
-        console.error("Couldn't register: " + key);
-        throw e;
-    }
+    for (var key in widgets.utilities) {
+        try {
+            registry.registerUtility(widgets.utilities[key]);
+        } catch (e) {
+            console.error("Couldn't register: " + key);
+            throw e;
+        }
     
+    }    
 }
