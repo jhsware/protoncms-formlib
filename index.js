40dae10e3637a907a30b0458bbcc6184b58c3d6c
jhsware <sebastian@urbantalk.se>
2015-07-16 17:01:46 +0200

    No need to expose, it is available through the registry

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
+require('./src/AutoForm');
 
 module.exports.form_field_widgets = require('./src/formInputWidgets');
 module.exports.registerAllWidgets = require('./src/registerAllWidgets');
-module.exports.AutoForm = require('./src/AutoForm');
 module.exports.CustomInputWidget = require('./src/CustomInputWidget');
 module.exports.mixins = {
     FieldValidationMixin: require('./src/mixins/FieldValidationMixin')
